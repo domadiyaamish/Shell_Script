0ac862733c746893b6b69f19aeab11e7d5bcda8c
amish <doamdiyaamish@gmail.com>
2023-06-07 14:28:27 +0530

    add a varaible file

--- second.sh
+++ second.sh
@@ -3,3 +3,5 @@
 echo -e "\033[0;31m fail message here"
 echo -e "\033[0;32m success message here"
 echo -e "\033[0;33m warning message here"
+
+sleep 2
---
+++ variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+name="amish doamdiya"
+age=21
+
+echo "i am ${name} and my age is ${age}"
+
+echo 'i am ${name} and my age is ${age}'
+
+echo "${name}${age}"
+sleep 2
