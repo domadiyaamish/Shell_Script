0a9dada6c64bb2d8019fd9c14df5daffbd9d4067
amish <doamdiyaamish@gmail.com>
2023-06-10 12:35:30 +0530

    if else is added

---
+++ if-set-or-not.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# name="amish"
+: ${name:?"please set a value"}
+echo "i am here"
+
+#command line argument 
+: ${name:?"please set a value"}
+echo "i am here"
---
+++ if_else.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+number=5
+if [ $number -eq 5 ]
+then 
+    echo "number is equal 5"
+else
+    echo "number is not equal to 5"
+    
+fi
+    
